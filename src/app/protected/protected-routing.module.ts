eeb677beff2d1199507040f78ebf781102dba63c
SergeMax <maxime.cansell@gmail.com>
2022-07-10 20:07:45 +0200

    ok protected component

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -4,20 +4,22 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { ParametersComponent } from './parameters/parameters/parameters.component';
 import { PlanningComponent } from './planning/planning/planning.component';
 import { ProfilComponent } from './profil/profil/profil.component';
+import { ProtectedComponent } from './protected.component';
 import { WorkdayComponent } from './workday/workday/workday.component';
 
 const routes: Routes = [
   {
-   path: 'app',
-   children: [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'parameters', component: ParametersComponent },
-    { path: 'planning', component: PlanningComponent },
-    { path: 'profil', component: ProfilComponent },
-    { path: 'workday', component: WorkdayComponent },
-   ]
+    path: 'app',
+    component: ProtectedComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'parameters', component: ParametersComponent },
+      { path: 'planning', component: PlanningComponent },
+      { path: 'profil', component: ProfilComponent },
+      { path: 'workday', component: WorkdayComponent },
+    ]
   }
- ];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
