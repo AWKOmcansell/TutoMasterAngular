255fe26158373d21c336debc74758ed3bf321eb8
SergeMax <maxime.cansell@gmail.com>
2022-07-10 18:25:14 +0200

    ok protected component

---
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,16 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+  
+@Component({
+ selector: 'wa-page-not-found',
+ templateUrl: './page-not-found.component.html',
+ styles: []
+})
+export class PageNotFoundComponent {
+  
+ constructor(private location: Location) { }
+  
+ back() {
+  this.location.back();
+ }
+}
